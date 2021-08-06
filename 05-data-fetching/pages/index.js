ed9761bf058ba0df915f028ad25cc1d58788e872
Chris Wren <chriswrendeveloper@gmail.com>
2021-08-06 19:46:19 +0100

    Page Pre-Rendering & Data Fetching

---
+++ 05-data-fetching/pages/index.js
@@ -0,0 +1,52 @@
+import path from "path";
+
+import Link from "next/link";
+
+// run second
+function HomePage(props) {
+  const { products } = props;
+
+  return (
+    <ul>
+      {products.map((product) => (
+        <li key={product.id}>
+          <Link href={`/products/${product.id}`}>{product.title}</Link>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+// run first: consider it that it's not on front side
+export async function getStaticProps() {
+  console.log("(Re-)Generating...");
+  const fs = require("fs").promises;
+
+  const filePath = path.join(process.cwd(), "data", "dummy-backend.json"); // current working directory, 'root'
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+
+  if (!data) {
+    return {
+      redirect: {
+        destination: "/no-data",
+      },
+    };
+  }
+
+  if (data.products.length === 0) {
+    return { notFound: true };
+  }
+
+  return {
+    props: {
+      products: data.products,
+    },
+    // Next.js will attempt to re-generate the page:
+    // - When a request comes in
+    // - At most once every 10 seconds
+    revalidate: 10,
+  };
+}
+
+export default HomePage;
---
+++ 05-data-fetching/pages/products/[pid].js
@@ -0,0 +1,64 @@
+import path from "path";
+
+import { Fragment } from "react";
+
+function ProductDetailPage(props) {
+  const { loadedProduct } = props;
+
+  if (!loadedProduct) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <Fragment>
+      <h1>{loadedProduct.title}</h1>
+      <p>{loadedProduct.description}</p>
+    </Fragment>
+  );
+}
+
+async function getData() {
+  const fs = require("fs").promises;
+  const filePath = path.join(process.cwd(), "data", "dummy-backend.json"); // current working directory, 'root'
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+
+  return data;
+}
+
+export async function getStaticProps(context) {
+  // console.log(JSON.stringify(context, null, 4));
+  const { params } = context;
+  const productId = params.pid;
+
+  const data = await getData();
+
+  const product = data.products.find((product) => product.id === productId);
+
+  if (!product) {
+    return { notFound: true };
+  }
+
+  return {
+    props: {
+      loadedProduct: product,
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  const data = await getData();
+
+  const ids = data.products.map((product) => product.id);
+
+  const pathWithParams = ids.map((id) => ({ params: { pid: id } }));
+
+  return {
+    paths: pathWithParams,
+    // fallback: false,
+    fallback: true,
+    // fallback: "blocking",
+  };
+}
+
+export default ProductDetailPage;
